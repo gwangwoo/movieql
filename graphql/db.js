9e9699c6a7089fd211b4e63b9a6c79e8c0b96f5a
gwangwoo <rhkddn5161@naver.com>
2020-01-26 19:56:27 +0900

    2020-01-26 chap6

---
+++ graphql/db.js
@@ -0,0 +1,43 @@
+export const people = [
+    {
+    id: "0",
+    name: "Nicolas",
+    age: 18,
+    gender: "female"
+  },
+    {
+    id: "1",
+    name: "Jisu",
+    age: 18,
+    gender: "female"
+  },
+  {
+    id: "2",
+    name: "Japan Guy",
+    age: 25,
+    gender: "male"
+  },
+  {
+    id: "3",
+    name: "JD",
+    age: 22,
+    gender: "male"
+  },
+  {
+    id: "4",
+    name: "GwangWoo",
+    age: 27,
+    gender: "male"
+  },
+  {
+    id: "5",
+    name: "flynn",
+    age: 25,
+    gender: "female"
+  }
+  ];
+
+export const getById = id => {
+    const filteredPeople = people.filter(person => id === people.id);
+    return filteredPeople[0];
+}
